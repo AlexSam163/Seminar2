8155bf69017ade9bd3c62fd70e98350447bdb20e
AlexSam <163metall163@gmail.com>
2022-06-13 13:36:54 +0400

    add task4

---
+++ Homework2/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+int firstNumber = Input("Введите первое число: ");
+int secondNumber = Input("Введите второе число: ");
+
+if(Square(firstNumber)==secondNumber || Square(secondNumber) == firstNumber)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+   Console.WriteLine("Нет"); 
+}
+
+int Square(int a)
+{
+    return a*a;
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
